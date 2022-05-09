0a78a5acd89eb3efb605c61fa824736f997a6a7c
Jerzy <jurekbalcer@gmail.com>
2022-05-09 21:05:14 +0200

    web app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const telegram = require('./telegram.js');
 const spotify = require('./spotify.js');
 const database = require('./database.js');
@@ -11,7 +12,7 @@ async function sendReleasesNotifications(offset) {
   const foundAlbums = await database.find(albums);
 
   // keep only unannounced albums
-  albums = albums.filter( (album) => !foundAlbums.includes(album))
+  albums = albums.filter((album) => !foundAlbums.includes(album))
 
   // send telegram notifications
   albums.forEach((album) => telegram.sendTelegramMessage(album));
@@ -20,21 +21,40 @@ async function sendReleasesNotifications(offset) {
   database.write(albums);
 }
 
+// set up web app
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+  res.send("https://t.me/rap_premiery");
+})
+
+// offset used for getting new releases from spotify
+// without it api limits could be exceeded
 // wrapped in object to pass by reference
-var offset = {value: 0};
+var offset = { value: 0 }; 
 
-// set interval for continous releases checking
-setInterval(()=>
-  {
-  sendReleasesNotifications(offset)
+app.listen(app.get('port'), function () {
 
-  if(offset.value < 9){
-    offset.value += 3;
-  }
-  else{
-    offset.value = 0;
-  }
+  // set interval for continous releases checking
+  setInterval(() => {
+    sendReleasesNotifications(offset)
+
+    // only 3 releases at once
+    if (offset.value < 9) {
+      offset.value += 3;
+    }
+    else {
+      offset.value = 0;
+    }
 
   }
-, 10000);
+    , 10000);
+
+})
+
+
+
+
 
